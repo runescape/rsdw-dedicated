b85bca00c81a628731b127818a398610aed3a59f
John Edwards <john.edwards@jagex.com>
2026-02-03 16:16:36 +0000

    feat: config templating (#2)
    feat: template server configuration file
    * Provide environment variables to template basic server settings
    * Update README

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
-# rsdw-dedicated
+[![CI](https://github.com/runescape/rsdw-dedicated/actions/workflows/docker-image.yml/badge.svg?branch=main)](https://github.com/runescape/rsdw-dedicated/actions/workflows/docker-image.yml) [![Build and Publish](https://github.com/runescape/rsdw-dedicated/actions/workflows/docker-publish.yml/badge.svg)](https://github.com/runescape/rsdw-dedicated/actions/workflows/docker-publish.yml)
+
+# RuneScape: Dragonwilds Dedicated Server
+
+This image provides a convenient [RuneScape: Dragonwilds Server](https://store.steampowered.com/app/1374490/RuneScape_Dragonwilds/), which will automatically download the latest stable version at launch.
+
+<img src="https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/1374490/a3e75918e979b6c5f51264be694efb0597e99879/header.jpg?t=1765818039" alt="logo" width="300"/></img>
+
+# How to use this image
+
+## Available Container Image Repositories
+
+* GitHub: `ghcr.io/runescape/rsdw-dedicated`
+
+## Hosting a simple game server
+
+Running using Docker:
+```console
+docker run -d --name=rsdw-dedicataed ghcr.io/runescape/rsdw-dedicated
+```
+
+## System Requirements
+
+Minimum system requirements are:
+
+* 2 CPU cores
+* 2GiB RAM
+* 8GiB of disk space
+
+# Configuration
+
+## Environment Variables
+Feel free to overwrite these environment variables, using -e (--env):
+
+### Server Configuration
+
+| Variable             | Type   | Default        | Description    |
+| -------------------- | ------ | -------------- | -------------- |
+| RSDW_SERVER_NAME     | string | rsdw-container | Name of server | 
+| RSDW_WORLD_NAME      | string | random         | Visible name of server in the Worlds browser |
+| RSDW_PASSWORD        | string | random         | Server password. Explicitly set to an empty string if no password is desierd. |
+| RSDW_ADMINS          | string |                | Comma separated list of user ids |
+| RSDW_ADMIN_PASSWORD  | string | random         | Server admin password. |
+| RSDW_ADDITIONAL_ARGS | string |                | Additional CLI arguments to be passed into RSDragonwildsServer.sh |
+
+> [!NOTE]
+> Environment variables with `random` default values are set to random strings each time the container starts.
+> See the container's standard output for the randomly generated value.
+> Explicitly setting these environment variables will disable this behaviour.
+
+## Debug Logging
+
+If you want to increase the verbosity of log output set the `DEBUG` environment variable:
+
+```dockerfile
+DEBUG=0                    (0=none, 1=steamcmd, 2=rsdw, 3=all)
+```
+
+## Validating Game Files
+
+If you break the game through your customisations and want steamcmd to validate and redownload then set the `STEAMAPPVALIDATE` environment variable to `1`:
+
+```dockerfile
+STEAMAPPVALIDATE=0          (0=skip validation, 1=validate game files)
+```
--- sniper/app/entry.sh
+++ sniper/app/entry.sh
@@ -3,7 +3,58 @@
 # Exit script on failure
 set -e
 
-# Debug
+# Functions
+
+function download() {
+
+  # Create App Dir
+  mkdir -p "${STEAMAPPDIR}" || true
+
+  # Download Game Server
+  if [[ -n "$DEVBUILD_PRESIGNED_URL" ]]; then
+    # Download Dev build (zip archive) if URL provided
+    curl -o build.zip "${DEVBUILD_PRESIGNED_URL}"
+    unzip build.zip -d "${STEAMAPPDIR}"
+  else
+    # Else, download live build from Steam
+    if [[ "$STEAMAPPVALIDATE" -eq 1 ]]; then
+        VALIDATE="validate"
+    else
+        VALIDATE=""
+    fi
+
+    ## SteamCMD can fail to download
+    ## Retry logic
+    MAX_ATTEMPTS=3
+    attempt=0
+    while [[ $steamcmd_rc != 0 ]] && [[ $attempt -lt $MAX_ATTEMPTS ]]; do
+        ((attempt+=1))
+        if [[ $attempt -gt 1 ]]; then
+            echo "Retrying SteamCMD, attempt ${attempt}"
+            # Stale appmanifest data can lead for HTTP 401 errors when requesting old
+            # files from SteamPipe CDN
+            echo "Removing steamapps (appmanifest data)..."
+            rm -rf "${STEAMAPPDIR}/steamapps"
+        fi
+        eval bash "${STEAMCMDDIR}/steamcmd.sh" "${STEAMCMD_SPEW}"\
+                                    +force_install_dir "${STEAMAPPDIR}" \
+                                    +@bClientTryRequestManifestWithoutCode 1 \
+                                    +login anonymous \
+                                    +app_update "${STEAMAPPID}" "${VALIDATE}"\
+                                    +quit
+        steamcmd_rc=$?
+    done
+
+    ## Exit if steamcmd fails
+    if [[ $steamcmd_rc != 0 ]]; then
+        exit $steamcmd_rc
+    fi
+  fi
+}
+
+# MAIN
+
+# Debug handling
 
 ## Steamcmd debugging
 if [[ $DEBUG -eq 1 ]] || [[ $DEBUG -eq 3 ]]; then
@@ -14,53 +65,33 @@ if [[ $DEBUG -eq 2 ]] || [[ $DEBUG -eq 3 ]]; then
     RSDW_LOG="on"
 fi
 
-# Create App Dir
-mkdir -p "${STEAMAPPDIR}" || true
-
-# Download Game Server
-if [[ -n "$DEVBUILD_PRESIGNED_URL" ]]; then
-  # Download Dev build (zip archive) if URL provided
-  curl -o build.zip "${DEVBUILD_PRESIGNED_URL}"
-  unzip build.zip -d "${STEAMAPPDIR}"
-else
-  # Else, download live build from Steam
-  if [[ "$STEAMAPPVALIDATE" -eq 1 ]]; then
-      VALIDATE="validate"
-  else
-      VALIDATE=""
-  fi
-
-  ## SteamCMD can fail to download
-  ## Retry logic
-  MAX_ATTEMPTS=3
-  attempt=0
-  while [[ $steamcmd_rc != 0 ]] && [[ $attempt -lt $MAX_ATTEMPTS ]]; do
-      ((attempt+=1))
-      if [[ $attempt -gt 1 ]]; then
-          echo "Retrying SteamCMD, attempt ${attempt}"
-          # Stale appmanifest data can lead for HTTP 401 errors when requesting old
-          # files from SteamPipe CDN
-          echo "Removing steamapps (appmanifest data)..."
-          rm -rf "${STEAMAPPDIR}/steamapps"
-      fi
-      eval bash "${STEAMCMDDIR}/steamcmd.sh" "${STEAMCMD_SPEW}"\
-                                  +force_install_dir "${STEAMAPPDIR}" \
-                                  +@bClientTryRequestManifestWithoutCode 1 \
-                                  +login anonymous \
-                                  +app_update "${STEAMAPPID}" "${VALIDATE}"\
-                                  +quit
-      steamcmd_rc=$?
-  done
-
-  ## Exit if steamcmd fails
-  if [[ $steamcmd_rc != 0 ]]; then
-      exit $steamcmd_rc
-  fi
-fi
-
 # FIX: steamclient.so fix
 mkdir -p ~/.steam/sdk64
 ln -sfT ${STEAMCMDDIR}/linux64/steamclient.so ~/.steam/sdk64/steamclient.so
+
+# Download Dedicated Server
+download
+
+# Parse Environment Variables
+
+## Generate random passwords, if required
+if [[ "$RSDW_PASSWORD" == "random" ]]; then
+  export RSDW_PASSWORD=$(pwgen -AB 12 1)
+  echo "RSDW_PASSWORD set to: ${RSDW_PASSWORD}"
+fi
+if [[ "$RSDW_ADMIN_PASSWORD" == "random" ]]; then
+  export RSDW_ADMIN_PASSWORD=$(pwgen -AB 12 1)
+  echo "RSDW_ADMIN_PASSWORD set to: ${RSDW_ADMIN_PASSWORD}"
+fi
+
+## Check that World name is set
+if [[ -z $RSDW_WORLD_NAME ]]; then
+  export RSDW_WORLD_NAME=$(shuf -n 1 /etc/default/DedicatedServer.names)
+  echo "RSDW_WORLD_NAME set to: ${RSDW_WORLD_NAME}"
+fi
+
+# Template configuration file
+envsubst < /etc/default/DedicatedServer.ini > ${STEAMAPPDIR}/RSDragonwilds/Saved/Config/LinuxServer/DedicatedServer.ini
 
 # Switch to server directory
 cd "${STEAMAPPDIR}/RSDragonwilds/"
